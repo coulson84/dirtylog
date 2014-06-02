2c151580974b4a524974a20ec4499e7e49335489
Ben Brunton <ben@flashtalking.com>
2014-06-02 21:45:02 +0100

    autoscroll

--- public/dirtylog.js
+++ public/dirtylog.js
@@ -1,12 +1,16 @@
 var socket = io();
 var user = document.querySelector('#user').value;
+var ul = document.querySelector('ul');
 
 socket.emit('register', user);
 
 socket.on('log', function(msg){
 	var li = document.createElement('li');
-	var ul = document.querySelector('ul');
 	li.innerHTML = msg;
 
 	ul.appendChild(li);
+	ul.scrollTop = ul.scrollHeight;
 });
+
+
+ul.scrollTop = ul.scrollHeight;
